36d9a921804b5dcf6d226e4aa6a73d47a9f19827
ronggh@sina.cn <ronggh@sina.cn>
2019-03-02 18:51:29 +0800

    bayes detect language

---
+++ classifier/bayes_language_detector.py
@@ -0,0 +1,50 @@
+"""
+用朴素贝叶斯做语种检测
+
+"""
+
+import re
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+
+class LanguageDetector():
+
+    def __init__(self, classifier=MultinomialNB()):
+        self.classifier = classifier
+        self.vectorizer = CountVectorizer(ngram_range=(1,2), max_features=1000, preprocessor=self._remove_noise)
+
+    def _remove_noise(self, document):
+        noise_pattern = re.compile("|".join(["http\S+", "\@\w+", "\#\w+"]))
+        clean_text = re.sub(noise_pattern, "", document)
+        return clean_text
+
+    def features(self, X):
+        return self.vectorizer.transform(X)
+
+    def fit(self, X, y):
+        self.vectorizer.fit(X)
+        self.classifier.fit(self.features(X), y)
+
+    def predict(self, x):
+        return self.classifier.predict(self.features([x]))
+
+    def score(self, X, y):
+        return self.classifier.score(self.features(X), y)
+
+
+if __name__=="__main__":
+    # 加载数据，训练，测试
+    in_f = open('../data/bayes_language_detector_data/data.csv', encoding='utf-8')
+    lines = in_f.readlines()
+    in_f.close()
+    dataset = [(line.strip()[:-3], line.strip()[-2:]) for line in lines]
+    x, y = zip(*dataset)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+
+    language_detector = LanguageDetector()
+    language_detector.fit(x_train, y_train)
+    print(language_detector.predict('This is an English sentence'))
+    print(language_detector.score(x_test, y_test))
